f421410d8e811fa111ce860a6a9a5d405229be61
emilioTM <emiliogallo80@gmail.com>
2024-06-07 23:22:00 -0300

    create api reset

--- blogdecafe_inicio/js/fetchApi.js
+++ blogdecafe_inicio/js/fetchApi.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+
+getResults()
+
+const url = 'https://visual-crossing-weather.p.rapidapi.com/forecast?contentType=csv&unitGroup=us&aggregateHours=24&location=Washington%2CDC%2CUSA&shortColumnNames=false';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'c8a1180a6fmsh023edf57775999bp16305djsnea67440b0915',
+		'x-rapidapi-host': 'visual-crossing-weather.p.rapidapi.com'
+	}
+};
+
+async function getResults(){
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+}
+getResults()
